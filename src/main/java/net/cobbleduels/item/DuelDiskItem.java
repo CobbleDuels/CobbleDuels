91621fbec0f75c22c70411c8f5f245ec5159b5e2
yagoTicketS <124682798+yagoTicketS@users.noreply.github.com>
2025-05-13 21:41:17 -0300

    itens setup and name change

---
+++ src/main/java/net/cobbleduels/item/DuelDiskItem.java
@@ -0,0 +1,9 @@
+package net.cobbleduels.item;
+
+import net.minecraft.world.item.Item;
+
+public class DuelDiskItem extends Item {
+    public DuelDiskItem() {
+        super(new Properties().stacksTo(1));
+    }
+}
